21d7d81380a7cf08c78260dfc9602e63aae45eb6
harmvandendorpel <mail@harmvandendorpel.com>
2016-09-29 21:24:21 +0200

    text fade

--- src/const/script.js
+++ src/const/script.js
@@ -1,9 +1,17 @@
 import SlideTypes from './slide-types';
 
 const script = [
+  { file: 'swf/dibbets.swf', type: SlideTypes.FLASH },
+  { type: SlideTypes.TEXT, text: 'When we break things down to their smallest parts, they will each seem dry as dust.' },
+  { type: SlideTypes.TEXT, text: 'As if some fundamental essence has been lost.' },
+  { type: SlideTypes.TEXT, text: 'Think of the classic brain in a vat Descartes spoke of.' },
+  { type: SlideTypes.TEXT, text: 'Think of the way in which the brain experiences the world.' },
+  { type: SlideTypes.TEXT, text: 'Creating something that already exists and presenting it as new is considered plagiarising.' },
+  { type: SlideTypes.TEXT, text: 'Creating something that already exists and presenting it as old is considered kitsch.' },
+  { file: 'swf/dibbets.swf', type: SlideTypes.FLASH },
   { type: SlideTypes.TEXT, text: 'It\'s the same brain. It\'s alive. It\'s undamaged.' },
   { type: SlideTypes.TEXT, text: 'In fact, it\'s completely untouched. We have not even opened the inner membranes.' },
-  { type: SlideTypes.TEXT, text: 'The big difference, of course, would be that we\'ve severed every single nerve that leads into it and this means that your thinking would no longer be influenced by your senses.' },
+  { type: SlideTypes.TEXT, text: 'The big difference, of course, would be that we have severed every single nerve that leads into it and this means that your thinking would no longer be influenced by your senses.' },
   { type: SlideTypes.TEXT, text: 'You\'d be living in an extraordinarily pure and detached world.' },
   { type: SlideTypes.TEXT, text: 'Nothing to bother you at all, not even pain. You could not possibly feel pain because there would not be any nerves to feel it with.' },
   { type: SlideTypes.TEXT, text: 'In a way, it would be an almost perfect situation. No worries or fears or pains or hunger or thirst.' },
@@ -15,7 +23,6 @@ const script = [
   { file: 'img/b.png', type: SlideTypes.IMAGE },
   { file: 'swf/drostescrolls.swf', type: SlideTypes.FLASH },
   { file: 'img/d.jpg', type: SlideTypes.IMAGE },
-  { file: 'swf/dibbets.swf', type: SlideTypes.FLASH },
   { file: 'img/c.jpg', type: SlideTypes.IMAGE },
   { file: 'swf/highway.swf', type: SlideTypes.FLASH },
   { file: 'swf/whiterectangle.swf', type: SlideTypes.FLASH },
--- src/screens/writing.js
+++ src/screens/writing.js
@@ -1,7 +1,9 @@
 import sha1 from 'sha1';
 import $ from 'jquery';
 import Screen from './abstract';
+
 const url = 'http://corenlp.run/?properties=%7B%22annotators%22%3A%20%22tokenize%2Cssplit%2Cpos%22%2C%20%22date%22%3A%20%222016-09-18T17%3A11%3A41%22%2C%20%22coref.md.type%22%3A%20%22dep%22%2C%20%22coref.mode%22%3A%20%22statistical%22%7D';
+const LOCALSTORAGE_ID = 'NLPCACHE';
 
 const CLASS_MAPPING = {
   ',': 'comma',
@@ -22,24 +24,32 @@ export default class WritingScreen extends Screen {
     this.$stage.append(this.$result);
     this.$screen.append(this.$stage);
 
-    this.cache = localStorage.getItem('NLPCACHE') ?
-      JSON.parse(localStorage.getItem('NLPCACHE')) : {};
+    this.cache = localStorage.getItem(LOCALSTORAGE_ID) ?
+      JSON.parse(localStorage.getItem(LOCALSTORAGE_ID)) : {};
 
     this.text = screenData.text;
-    this.updateText();
+
+    this.tokenIndex = 0;
+    this.getData(this.text).then((result) => {
+      this.result = result;
+
+      this.redraw();
+    });
   }
 
-  getData(text, callback) {
+  getData(text) {
     const hash = sha1(text);
-    if (this.cache[hash]) {
-      callback(this.cache[hash]);
-    } else {
-      $.post(url, text, (response) => {
-        this.cache[hash] = response;
-        localStorage.setItem('NLPCACHE', JSON.stringify(this.cache));
-        callback(response);
-      });
-    }
+    return new Promise((resolve) => {
+      if (this.cache[hash]) {
+        resolve(this.cache[hash]);
+      } else {
+        $.post(url, text, (response) => {
+          this.cache[hash] = response;
+          localStorage.setItem(LOCALSTORAGE_ID, JSON.stringify(this.cache));
+          resolve(response);
+        });
+      }
+    });
   }
 
   decideClass(pos) {
@@ -47,29 +57,63 @@ export default class WritingScreen extends Screen {
     return `class-${mapping}`;
   }
 
-  displaySentence(sentence) {
-    sentence.tokens.forEach((token) => {
-      if (token.before === '↵') {
-        this.$result.append($('<br>'));
-      }
-      const $word = $('<span></span>')
-        .text(token.originalText)
-        .addClass(this.decideClass(token.pos));
+  displayToken(token) {
+    const $word = $('<span></span>')
+      .text(token.originalText)
+      .addClass('token')
+      .addClass(this.decideClass(token.pos));
 
-      this.$result.append($word);
-      if (token.after === ' ') {
-        $word.addClass('space');
-      }
-    });
+    this.$result.append($word);
+    setTimeout(() => $word.addClass('show'), 50);
+    if (token.after === ' ') {
+      $word.addClass('space');
+    }
   }
 
-  displayResult(result) {
-    result.sentences.forEach((sentence) => {
-      this.displaySentence(sentence);
-    });
+  wait() {
+    const token = this.tokens[this.tokenIndex];
+    const text = token.originalText;
+    let duration = null;
+
+    switch (text) {
+      case ',':
+        duration = 2000;
+        break;
+
+      case '.':
+        duration = 3000;
+        break;
+
+      default:
+        duration = text.length * 150 + 200;
+    }
+
+    this.displayToken(token);
+    this.tokenIndex++;
+
+    this.lastTimeoutId = setTimeout(() => {
+      this.lastTimeoutId = null;
+      if (this.tokenIndex < this.tokens.length) {
+        this.wait();
+      }
+    }, duration);
   }
 
-  updateText() {
-    this.getData(this.text, (result) => this.displayResult(result));
+  redraw() {
+    this.tokens = [];
+    this.result.sentences.forEach((sentence) => {
+      sentence.tokens.forEach(token => this.tokens.push(token));
+    });
+
+    setTimeout(() => {
+      this.wait();
+    }, 5000);
+  }
+
+  destroy() {
+    super.destroy();
+    if (this.lastTimeoutId) {
+      clearTimeout(this.lastTimeoutId);
+    }
   }
 }
