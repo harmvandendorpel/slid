6c8b57ca6f022086e05f48112ff31898b79a8e18
harmvandendorpel <mail@harmvandendorpel.com>
2017-04-11 11:55:53 +0200

    impakt changes

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.join(__dirname, 'public/js'),
     filename: 'bundle.js'
   },
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   module: {
     loaders: [
       {
